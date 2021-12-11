f093b26f9ecffc7e9283e7795698f56f028bcd91
Dhavale <kalyani-rajaram.dhavale@capgemini.com>
2021-12-11 16:47:05 +0530

    ngo

---
+++ src/components/admin.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+class Admin extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Admin;
---
+++ src/components/employee.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class Employee extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>Employee page</div>
+         );
+    }
+}
+ 
+export default Employee ;
